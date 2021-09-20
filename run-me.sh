518574f158d7668bb3c29a372428d20f8626e6ee
David Gardiner <dmgardiner25@gmail.com>
2021-09-20 11:46:40 -0700

    Update run-me.sh

--- run-me.sh
+++ run-me.sh
@@ -2,4 +2,4 @@
 
 echo -n "I was triggered by the postcreate" >> ./postcreated.txt
 git add ./postcreated.txt
-git commit -m "I am a git commit"
+git commit -m "I am a git commit" # Don't remove, this will break the GitStatusAsserts!
