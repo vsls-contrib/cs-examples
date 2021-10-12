3fb113bbf6e06ad68e4647b27ab81283f15abb51
Aditya Kumar <kumarad@microsoft.com>
2021-10-12 02:42:18 +0000

    Add failure test branch for prebuild on create

--- on-create-command.sh
+++ on-create-command.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-echo -n "I was triggered by the oncreate" >> ./oncreate.txt
+echo -n "I was triggered by the oncreate but failed" >> ./oncreate.txt
 git add ./oncreate.txt
 git commit -m "I am a git commit 2" # Don't remove, this will break the GitStatusAsserts!
+exit 1
