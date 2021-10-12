50b027574ce4b4ed359fdd14aa1ee3bc5f1bf35a
Aditya Kumar <kumarad@microsoft.com>
2021-10-12 16:39:31 +0000

    Add oncreate echo statement to check in the creation log file

--- on-create-command.sh
+++ on-create-command.sh
@@ -3,3 +3,4 @@
 echo -n "I was triggered by the oncreate" >> ./oncreate.txt
 git add ./oncreate.txt
 git commit -m "I am a git commit 2" # Don't remove, this will break the GitStatusAsserts!
+echo -n "OnCreateCommand should be in creation log"
