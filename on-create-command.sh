becf2a97adddd6fe10982f7a8f4ff9504a55b99d
Aditya Kumar <kumarad@microsoft.com>
2021-10-12 17:07:59 +0000

    Termiinal log to oncreate

--- on-create-command.sh
+++ on-create-command.sh
@@ -3,4 +3,5 @@
 echo -n "I was triggered by the oncreate but failed" >> ./oncreate.txt
 git add ./oncreate.txt
 git commit -m "I am a git commit 2" # Don't remove, this will break the GitStatusAsserts!
+echo -n "I was triggered by the oncreate but failed"
 exit 1
