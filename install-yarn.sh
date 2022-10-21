e0bb74cc61c7572cad552669d8866ef8812b951e
David Gardiner <dmgardiner25@github.com>
2022-10-21 15:09:47 -0700

    Move script to repo root

--- install-yarn.sh
+++ install-yarn.sh
@@ -3,5 +3,4 @@ curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 apt update
 apt install -y yarn
-cd ..
 yarn
