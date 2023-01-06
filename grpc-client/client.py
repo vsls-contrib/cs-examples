c6e88042d9e0e6d54d1d0d606cec35c894070b09
David Gardiner <dmgardiner25@github.com>
2023-01-06 18:21:22 +0000

    Fix error

--- grpc-client/client.py
+++ grpc-client/client.py
@@ -5,7 +5,7 @@ import proto_pb2_grpc
 print("Starting client and sending request")
 
 with grpc.insecure_channel('unix:///workspaces/.codespaces/shared/cs-agent.sock') as channel:
-    stub = proto_pb2_grpc.CodespaceHostStub(channel, metadata)
+    stub = proto_pb2_grpc.CodespaceHostStub(channel)
     metadata = (('Authorization', 'Bearer token'))
     response = stub.ExportLogsAsync(request=proto_pb2.ExportLogsRequest(), metadata=metadata)
 
