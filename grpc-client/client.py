4dd4af98cd43c616fa86b117385783cf8d78832b
David Gardiner <dmgardiner25@github.com>
2023-01-06 18:59:10 +0000

    Don't automatically install dependencies

--- grpc-client/client.py
+++ grpc-client/client.py
@@ -2,11 +2,11 @@ import grpc
 import proto_pb2
 import proto_pb2_grpc
 
-print("Starting client and sending request")
+print("Starting client and sending request...")
 
 with grpc.insecure_channel('unix:///workspaces/.codespaces/shared/cs-agent.sock') as channel:
     stub = proto_pb2_grpc.CodespaceHostStub(channel)
     metadata = (('Authorization', 'Bearer token'))
-    response = stub.ExportLogsAsync(request=proto_pb2.ExportLogsRequest(), metadata=metadata)
+    response = stub.ExportLogsAsync(proto_pb2.ExportLogsRequest(), metadata=(('authorization','Bearer token'),))
 
-print("Received " + response.FilePath)
+print("Logs file path: " + response.FilePath)
