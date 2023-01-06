6b09769bd8ac61c68fa6aa053110a3e7d4c85aac
David Gardiner <dmgardiner25@github.com>
2023-01-06 18:17:59 +0000

    Add metadata to request

--- grpc-client/client.py
+++ grpc-client/client.py
@@ -5,7 +5,8 @@ import proto_pb2_grpc
 print("Starting client and sending request")
 
 with grpc.insecure_channel('unix:///workspaces/.codespaces/shared/cs-agent.sock') as channel:
-    stub = proto_pb2_grpc.CodespaceHostStub(channel)
-    response = stub.ExportLogsAsync(proto_pb2.ExportLogsRequest())
+    stub = proto_pb2_grpc.CodespaceHostStub(channel, metadata)
+    metadata = (('Authorization', 'Bearer token'))
+    response = stub.ExportLogsAsync(request=proto_pb2.ExportLogsRequest(), metadata=metadata)
 
 print("Received " + response.FilePath)
