851f47a8d9c664a83a3e3ccae314eb88f22e7db0
Josh Spicer <joshspicer@github.com>
2023-08-16 17:49:05 +0000

    test case for dev containers with ${localWorkspaceFolderBasename} in their workspaceFolder

--- grpc-client/install_dependencies.sh
+++
@@ -1,3 +0,0 @@
-apt update
-apt install -y python3-pip
-pip3 install grpcio==1.56.2 grpcio-tools==1.56.2
--- run-me.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo -n "I was triggered by the postcreate" >> ./postcreated.txt
-git add ./postcreated.txt
-git commit -m "I am a git commit" # Don't remove, this will break the GitStatusAsserts!
